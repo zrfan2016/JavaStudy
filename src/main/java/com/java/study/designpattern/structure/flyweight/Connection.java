45246d8390d525c1bf19f6ec46c71e2a747cb2fa
zrfan <zrfan2016@qq.com>
2020-03-18 22:35:28 +0800

    0318 update

--- src/main/java/com/java/study/designpattern/structure/flyweight/Connection.java
+++ src/main/java/com/java/study/designpattern/structure/flyweight/Connection.java
@@ -1,5 +1,7 @@
 package com.java.study.designpattern.structure.flyweight;
 
+import java.util.UUID;
+
 /**
  * @author zrfan
  * @className Connection
@@ -8,10 +10,21 @@ package com.java.study.designpattern.structure.flyweight;
  **/
 public class Connection {
 
-    private String url;
-    private int port;
-    private String userName;
-    private String password;
+    private String id;
 
+    public Connection() {
+        this.id = UUID.randomUUID().toString().toUpperCase();
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Connection{");
+        sb.append("id='").append(id).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
